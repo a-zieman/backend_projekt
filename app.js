531bb5d0f9cdf54f9312edeb8c9dc7487a32bdeb
a-zieman <ania.zieman@gmail.com>
2023-12-09 14:06:32 +0100

    Connection to mongoDB

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-const express = require("express");
+const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
+// Connect to datatbase
+mongoose.
+connect('mongodb+srv://admin:PASSWORD@backendprojekt.run5ycn.mongodb.net/Node-API?retryWrites=true&w=majority')
+.then(() => {
+  console.log('Connected to MongoDB')
+  app.listen(3000,() => {
+    console.log("Node API app is running on port 3000")
+  });
+}).catch(() =>{
+  console.log(error)
+})
+
+//routes
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
-
-app.listen(3000,() => console.log("Server listening at port 3000"));
